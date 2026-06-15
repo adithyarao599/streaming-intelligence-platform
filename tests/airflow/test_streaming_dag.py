126786840835b7f194760d3d074483a34de28bd8
Adithya Rao J <adithyarao599@gmail.com>
2026-06-15 21:54:09 +0000

    Fix Airflow DAG loading in CI

--- tests/airflow/test_streaming_dag.py
+++ tests/airflow/test_streaming_dag.py
@@ -1,8 +1,15 @@
+from pathlib import Path
+
 from airflow.models import DagBag
 
 
 def test_streaming_dag():
-    dagbag = DagBag(include_examples=False)
+    dag_folder = str(Path(__file__).resolve().parents[2] / "dags")
+
+    dagbag = DagBag(
+        dag_folder=dag_folder,
+        include_examples=False,
+    )
 
     assert len(dagbag.import_errors) == 0
     assert "streaming_platform" in dagbag.dags
